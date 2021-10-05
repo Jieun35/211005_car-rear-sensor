20ef2be1b3cc6408ac5d738d6b89d9c4ce565537
Jieun35 <90733575+Jieun35@users.noreply.github.com>
2021-10-05 11:50:36 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.ts
@@ -0,0 +1,25 @@
+let cm = 0
+basic.forever(function () {
+    cm = 0
+    basic.pause(100)
+})
+basic.forever(function () {
+    if (cm < 10) {
+        music.playTone(262, 100)
+    } else if (10 < cm && cm < 30) {
+        music.playTone(262, 200)
+    } else if (31 < cm && cm < 100) {
+        music.playTone(262, 700)
+    } else {
+        music.stopAllSounds()
+        music.stopMelody(MelodyStopOptions.All)
+    }
+})
+basic.forever(function () {
+    cm = sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    )
+    basic.pause(100)
+})
